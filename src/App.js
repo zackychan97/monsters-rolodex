0f66b6edda4792ccdff76261d5320d154ed09f09
Zack Bumpous <upwork.ztb@gmail.com>
2020-07-11 10:46:32 -0500

    Modifies gitignore to ignore node_modules. Adds the lifecycle method componentDidMount() to our App component in App.js. The code executed in this componentDidMount() lifecycle method fetches a jsonplaceholder api of 10 users, followed by .then getting the response and turning it into .json(), then getting the users and using this.setState to set our monsters array to users.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  // Since we are using a class, we can create a way to access State:
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      // fetch gives us a promise, a promise of the response data
+      // so we use .json() to give us that response in json so our javascript can understand it
+      .then((response) => response.json())
+      // then that will get returned to us as a new promise
+      // which we will now have as the body, which is the users
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* map() has 2 args, the state we want (monsters),   */}
+        {this.state.monsters.map((monsters) => (
+          <h1 key={monsters.id}>{monsters.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
